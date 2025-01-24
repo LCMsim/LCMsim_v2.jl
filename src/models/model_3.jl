938bdedeaefc333f48d6d09c1f577fe25d00fcd2
Christof Obertscheider <christof.obertscheider@fhwn.ac.at>
2025-01-24 14:53:11 +0100

    Start of development of i_model=3 (VARI)

--- src/models/model_3.jl
+++ src/models/model_3.jl
@@ -10,6 +10,9 @@ function update_rho(
     #Add the modified equation for variable porosity
     rho_new = (props.porosity * rho_old - Δt * F_rho_num / props.volume - rho_old * (props.porosity - cellporositytimescellporosityfactor_old)) / props.porosity
 
+    #Model_2
+    #rho_new = (props.porosity * rho_old - Δt * F_rho_num / props.volume) / props.porosity
+
     return max(rho_new, 0.0)
 end
 
@@ -30,30 +33,43 @@ function scale_properties(
     iter::Int
     )::ScaledProperties
 
-    porosity_val = cell.ap + cell.cp * (p_old ^ 2)
-    target_porosity_val = (1 - cell.porosity) / (1 - porosity_val) * porosity_val
-    if iter == 1 # TODO move this to initialization
-        target_porosity_val = target_porosity_val
-    else
-        target_porosity_val = porosity_times_porosity_old + 0.01 * (target_porosity_val - porosity_times_porosity_old)
-    end
-    porosity_factor = target_porosity_val / cell.porosity
-    permeability_factor = (1 - (1 - porosity_val)) ^ 3 / (1 - porosity_val) ^ 2
+#    porosity_val = cell.ap + cell.cp * (p_old ^ 2)
+#    target_porosity_val = (1 - cell.porosity) / (1 - porosity_val) * porosity_val
+#    if iter == 1 # TODO move this to initialization
+#        target_porosity_val = target_porosity_val
+#    else
+#        target_porosity_val = porosity_times_porosity_old + 0.01 * (target_porosity_val - porosity_times_porosity_old)
+#    end
+#    porosity_factor = target_porosity_val / cell.porosity
+#    permeability_factor = (1 - (1 - porosity_val)) ^ 3 / (1 - porosity_val) ^ 2
+#
+#    thickness_factor = 1.0
+#    volume_factor = 1.0
+#    face_factor = 1.0   
+#    viscosity_factor = 1.0
+#    
+#    faces = [x.face_area for x in cell.neighbours]
+#
+#    return ScaledProperties(
+#        cell.thickness * thickness_factor,
+#        cell.volume * volume_factor,
+#        faces .* face_factor,
+#        cell.porosity * porosity_factor,
+#        cell.permeability * permeability_factor,
+#        viscosity * viscosity_factor,
+#        cell.alpha
+#    )
 
-    thickness_factor = 1.0
-    volume_factor = 1.0
-    face_factor = 1.0   
-    viscosity_factor = 1.0
-    
+
     faces = [x.face_area for x in cell.neighbours]
 
     return ScaledProperties(
-        cell.thickness * thickness_factor,
-        cell.volume * volume_factor,
-        faces .* face_factor,
-        cell.porosity * porosity_factor,
-        cell.permeability * permeability_factor,
-        viscosity * viscosity_factor,
+        cell.thickness,
+        cell.volume,
+        faces,
+        cell.porosity,
+        cell.permeability,
+        viscosity,
         cell.alpha
     )
 end
--- test/run_case1.jl
+++ test/run_case1.jl
@@ -5,15 +5,15 @@ savepath = joinpath(mypath,"test")
 meshfile = joinpath(mypath,"test","mesh_1.dat")
 partfile = joinpath(mypath,"test","part_description_1.csv")
 simfile = joinpath(mypath,"test","simulation_params_1.csv")
+i_model=3
+t_max = 200.
 
 @info "mypath = $mypath"
 @info "meshfile = $meshfile"
 @info "partfile = $partfile"
 @info "simfile = $simfile"
 
-t_max = 200.
 t_step = t_max/16
-i_model=2
 if i_model == 1
     modeltype = LCMsim_v2.model_1
 elseif i_model == 2         
@@ -27,8 +27,7 @@ meshfilename_parts=splitpath(meshfile)
 meshfilename_parts[end]="_" * meshfilename_parts[end]
 writefilename=joinpath(meshfilename_parts)
 
-
 LCMsim_v2.create_and_solve(savepath,meshfile,partfile,simfile,modeltype,t_max,t_step,LCMsim_v2.verbose,true,true)
 
-include("plot_case1.jl")
+include("plot_case1.jl");
 
