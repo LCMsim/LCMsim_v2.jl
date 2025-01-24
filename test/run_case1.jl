e28ffacf81e224018d0fdbb9983fd206b9e6c1ae
Christof Obertscheider <christof.obertscheider@fhwn.ac.at>
2025-01-24 13:48:19 +0100

    Test for case 1 added

---
+++ test/run_case1.jl
@@ -0,0 +1,34 @@
+include("../src/LCMsim_v2.jl")
+
+mypath=pwd()
+savepath = joinpath(mypath,"test")
+meshfile = joinpath(mypath,"test","mesh_1.dat")
+partfile = joinpath(mypath,"test","part_description_1.csv")
+simfile = joinpath(mypath,"test","simulation_params_1.csv")
+
+@info "mypath = $mypath"
+@info "meshfile = $meshfile"
+@info "partfile = $partfile"
+@info "simfile = $simfile"
+
+t_max = 200.
+t_step = t_max/16
+i_model=2
+if i_model == 1
+    modeltype = LCMsim_v2.model_1
+elseif i_model == 2         
+    modeltype = LCMsim_v2.model_2
+else
+    modeltype = LCMsim_v2.model_3
+end  
+
+filename_parts=splitpath(meshfile)
+meshfilename_parts=splitpath(meshfile)
+meshfilename_parts[end]="_" * meshfilename_parts[end]
+writefilename=joinpath(meshfilename_parts)
+
+
+LCMsim_v2.create_and_solve(savepath,meshfile,partfile,simfile,modeltype,t_max,t_step,LCMsim_v2.verbose,true,true)
+
+include("plot_case1.jl")
+
